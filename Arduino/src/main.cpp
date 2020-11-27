a1028a324d97621b02411f49d754161ed7b5b716
smallker <wahyu.smallker@gmail.com>
2020-11-27 12:52:13 +0700

    micro bar

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -5,7 +5,7 @@
 #include <TimerOne.h>
 #define RX_ARD 3
 #define TX_ARD 2
-
+#define SENSOR_PIN A0
 SoftwareSerial node(3, 2);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 const byte ROWS = 4; //four rows
@@ -23,51 +23,148 @@ unsigned long timer_t = 0;
 char buff[5];
 int count;
 char key;
-volatile bool sent;
+volatile bool sent, sentfreq, is5Khz, reset;
+
+String serialData;
+float I = 0.2, V;
+int W, H, A, Z;
+float tbw, ecw;
+
 void readKeypad()
 {
   key = kpd.getKey();
-  if(key == '#') sent = true;
+  if (key == '#')
+    sent = true;
+  if (key == '*')
+  {
+    sent = false;
+    sentfreq = false;
+    is5Khz = false;
+    reset = true;
+  }
+  if (key && key != '#' && key != '*')
+  {
+    buff[count] = key;
+    count++;
+  }
+}
+void measure(String data)
+{
+  char string[data.length() + 1];
+  data.toCharArray(string, data.length() + 1);
+  char *token = strtok(string, ",");
+  int arr[4];
+  int c;
+  while (token != NULL)
+  {
+    arr[c] = atoi(token);
+    c++;
+    token = strtok(NULL, ",");
+  }
+  /*
+    sampling 20 data, ganti delay sesuai sampling rate yg dibutuhkan
+  */
+  float sampling;
+  for (int i = 0; i < 20; i++)
+  {
+    float sensorReading = analogRead(SENSOR_PIN);
+    sampling = sampling + sensorReading;
+    Serial.println((String)i + " => " + (String)sensorReading);
+    delay(20);
+  }
+  V = sampling / 20;
+  Z = V / I;
+  W = arr[0];
+  H = arr[1];
+  A = arr[2];
+  if (is5Khz)
+  {
+    ecw = 2.53 + 0.118903 * H * H / Z + 0.06753 * W - 0.02 * A;
+  }
+  else
+  {
+    tbw = 6.53 + 0.36740 * H * H / Z + 0.17531 * W - 0.11 * A;
+  }
+  Serial.println("sampling : " + (String)sampling + " V : " + (String)V);
+  Serial.println("berat : " + (String)W + " tinggi : " + (String)H + " usia : " + (String)A + " ecw : " + (String)ecw + " tbw : " + (String)tbw);
 }
 void setup()
 {
   Serial.begin(9600);
   node.begin(9600);
+  pinMode(SENSOR_PIN, INPUT);
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.print("No. Pasien :");
   kpd.setDebounceTime(50);
   Timer1.initialize(1000);
   Timer1.attachInterrupt(readKeypad);
 }
 void loop()
 {
-  if (key && key != '#')
+  if (sentfreq)
   {
-    buff[count] = key;
-    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Frekuensi :");
+    lcd.setCursor(0, 1);
+    lcd.print(buff);
+  }
+  else
+  {
     lcd.setCursor(0, 0);
     lcd.print("No. Pasien :");
     lcd.setCursor(0, 1);
     lcd.print(buff);
-    count++;
   }
+
   if (sent)
   {
-    node.print(buff);
-    lcd.clear();
-    lcd.print("Tunggu . .");
+    if (sentfreq)
+    {
+      if (((String)buff) == "5")
+      {
+        is5Khz = true;
+      }
+      else
+        is5Khz = false;
+      measure(serialData);
+      is5Khz ? node.print("f1" + (String)ecw) : node.print("f0" + (String)tbw);
+    }
+    else
+    {
+      lcd.clear();
+      lcd.print("Tunggu . .");
+      node.print("d" + (String)buff);
+    }
+
     while (true)
     {
       if (node.available() > 0)
       {
-        String data = node.readStringUntil(13);
-        if (data.length() > 0)
+        sentfreq = true;
+        sent = false;
+        serialData = node.readStringUntil(13);
+        if (serialData.length() > 0)
         {
           lcd.clear();
-          lcd.print(data);
-          delay(1000);
+          if (serialData == "na")
+          {
+            lcd.setCursor(0, 0);
+            lcd.print("tidak ditemukan");
+          }
+          if (serialData == "f")
+          {
+            lcd.clear();
+            lcd.setCursor(0, 0);
+            is5Khz ? lcd.print("ECW : " + (String)ecw + " L") : lcd.print("TBW : " + (String)tbw + " L");
+            while (true)
+            {
+              if (reset == true)
+              {
+                reset = false;
+                break;
+              }
+            }
+          }
           break;
         }
       }
@@ -78,7 +175,5 @@ void loop()
       buff[i] = 0;
     }
     lcd.clear();
-    lcd.print("Frekuensi :");
-    sent = false;
   }
 }
--- server/index.js
+++ server/index.js
@@ -8,9 +8,9 @@ var ip = require('ip');
 var mysql = require('mysql')
 var db = mysql.createConnection({
     host: "127.0.0.1",
-    user: "trial",
-    password: "trial123",
-    database: "sistem_kontrol"
+    user: "root",
+    password: "",
+    database: "bia"
 });
 db.connect((err) => {
     if (err) throw err
@@ -18,109 +18,51 @@ db.connect((err) => {
 })
 app.use(cors())
 app.use(bodyParser.json())
-app.use(express.static(path.join(__dirname, '../web/build/web')))
-app.use(express.urlencoded({ extended: true }))
-app.get('/', (req, res) => {
-    res.render('index')
-})
+// app.use(express.static(path.join(__dirname, '../web/build/web')))
+// app.use(express.urlencoded({ extended: true }))
+// app.get('/', (req, res) => {
+//     res.render('index')
+// })
 
-app.get('/sensor', (req, res) => {
-    db.query('select temp,hum,air,press from sensor order by id desc limit 1', (err, result, field) => {
-        res.send(result)
-    })
-})
-app.get('/relay', (req, res) => {
-    var relayId = req.query.id
-    db.query(`select ${req.query.id} from relay`, (err, result, field) => {
-        var state = result[0][relayId]
-        if (state == 0) {
-            res.send('0')
-        }
-        else {
-            res.send('1')
+app.post('/getperson', (req, res) => {
+    let no_pasien = req.body.no_pasien
+    db.query(`select * from pasien where no_pasien=${no_pasien}`, (err, result, field) => {
+        // res.send(result[0])
+        try {
+            let response = `${result[0].berat},${result[0].tinggi},${result[0].usia}`
+            res.send(response)
+        } catch (error) {
+            console.log(error)
+            res.status(404).send('na')
         }
     })
 })
-app.get('/startrelay', (req, res) => {
-    var relayId = req.query.id
-    db.query(`select ${req.query.id} from relay`, (err, result, field) => {
-        var state = result[0][relayId]
-        if (state == 0) {
-            db.query(`update relay set ${relayId} = 1`)
-            res.send('1')
-        }
-        else {
-            db.query(`update relay set ${relayId} = 0`)
-            res.send('0')
-        }
-    })
+app.post('/ecw', (req,res) => {
+    let no_pasien = req.body.no_pasien
+    let ecw = req.body.ecw
+    db.query(`update pasien set ecw=${ecw} where no_pasien = ${no_pasien}`)
+    res.send(`ECW : ${ecw}`)
 })
-app.get('/noderelay',(req,res)=>{
-    db.query(`select relay1,relay2,relay3,relay4 from relay`,(err,result,field)=>{
-        res.send(result[0])
-    })
+app.post('/tbw', (req,res) => {
+    let no_pasien = req.body.no_pasien
+    let tbw = req.body.tbw
+    db.query(`update pasien set tbw=${tbw} where no_pasien = ${no_pasien}`)
+    res.send(`TBW : ${tbw}`)
 })
-app.get('/poweroff', (req, res) => {
-    console.log('got request')
-    db.query(`update relay set relay1=0,relay2=0,relay3=0,relay4=0`, (err, result, field) => {
-        console.log(result)
-        res.send('OK')
-    })
-    
-})
-app.get('/jadwal', (req, res) => {
-    db.query('select timestamp,state from jadwal', (err, result, field) => {
-        res.send(result)
-    })
-})
-
-app.get('/cekjadwal', (req, res) => {
-    const minute = 60000
-    var now = Date.now()
-    var todo = now + (minute * 5)
-    db.query(`select timestamp,state from jadwal where timestamp < ${todo} and timestamp > ${now}`,(_err,_result,_field)=> {
-        if (_result.length > 0) {
-            res.send(_result)
-        }
-        else res.send('NA')
-    })
-})
-app.get('/hapusjadwal', (req, res) => {
-    let timestamp = req.query.timestamp
-    db.query(`delete from jadwal where timestamp = ${timestamp}`, (err, result, field) => {
-        res.send('ok')
-        console.log('jadwal terhapus')
-    })
-})
-app.get('/nodemcu', (req, res) => {
-    var temp = req.query.temp
-    var hum = req.query.hum
-    var press = req.query.press
-    var air = req.query.air
-    db.query(`insert into sensor (temp,hum,press,air) value (${temp},${hum},${press},${air})`)
-    res.send('OK')
-})
-app.post('/jadwal', (req, res) => {
-    let state = parseInt(req.query.state)
-    let timestamp = req.body.timestamp
-    db.query(`insert into jadwal (timestamp,state) value (${timestamp},${state})`)
-    res.send('OK')
-})
-
-cron.schedule('* * * * * *', () => {
-    var now = Date.now()
-    db.query(`select timestamp from jadwal where timestamp < ${now} AND state = 0`, (err, rows, field) => {
-        if (rows.length > 0) {
-            db.query(`update relay set relay1=0,relay2=0,relay3=0,relay4=0`)
-            db.query(`delete from jadwal where timestamp < ${now} AND state = 0`)
-        }
-    })
-    db.query(`select timestamp from jadwal where timestamp < ${now} AND state = 1`, (err, rows, field) => {
-        if (rows.length > 0) {
-            db.query(`update relay set relay1=1,relay2=1,relay3=1,relay4=1`)
-            db.query(`delete from jadwal where timestamp < ${now} AND state = 1`)
-        }
-    })
-})
+// cron.schedule('* * * * * *', () => {
+//     var now = Date.now()
+//     db.query(`select timestamp from jadwal where timestamp < ${now} AND state = 0`, (err, rows, field) => {
+//         if (rows.length > 0) {
+//             db.query(`update relay set relay1=0,relay2=0,relay3=0,relay4=0`)
+//             db.query(`delete from jadwal where timestamp < ${now} AND state = 0`)
+//         }
+//     })
+//     db.query(`select timestamp from jadwal where timestamp < ${now} AND state = 1`, (err, rows, field) => {
+//         if (rows.length > 0) {
+//             db.query(`update relay set relay1=1,relay2=1,relay3=1,relay4=1`)
+//             db.query(`delete from jadwal where timestamp < ${now} AND state = 1`)
+//         }
+//     })
+// })
 
 app.listen(3000, console.log(`Web Server : ${ip.address()}:3000`))
